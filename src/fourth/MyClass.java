d289b7f425e0c7cedd3d44bb0214c8bb12b8eff8
Morronel <zahar.kogut@gmail.com>
2018-10-13 01:44:52 +0300

    Eto moi commentarii №2

--- src/fourth/MyClass.java
+++ src/fourth/MyClass.java
@@ -1,7 +1,30 @@
 package fourth;
 
+import java.util.ArrayList;
+
 public class MyClass {
+
+    public static void combine(ArrayList<Integer> first, ArrayList<Integer> second){
+        for (int i = 0; i < second.size(); i++) {
+            first.add(second.get(i));
+        }
+    }
+
+    public static void pritner(ArrayList<Integer> list){
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        ArrayList<Integer> uno = new ArrayList<>();
+        ArrayList<Integer> dos = new ArrayList<>();
+
+        uno.add(1);
+        uno.add(2);
+        dos.add(7);
+        dos.add(5);
+        combine(uno, dos);
+        pritner(uno);
     }
 }
